32210572c05f2e21b0949755bb1d341b0daeb0de
andrew tomich <johnatomich@gmail.com>
2021-10-14 15:45:08 -0500

    .gitignore and update app.py

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+#from flask_migrate import Migrate
+import os
 
 
 app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
+#from models import PipeLine1
+#migrate = Migrate(app, db)
 
 @app.route('/')
 def hello():
@@ -11,3 +19,4 @@ def hello():
 
 if __name__ == "__main__":
     app.run()
+
