7c4495e666afe7489a3c64b46f6325016a5203ba
andrew tomich <johnatomich@gmail.com>
2021-10-17 15:56:57 -0500

    Reset database to fix migration

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-#from flask_migrate import Migrate
+from flask_migrate import Migrate
 import os
 
 
@@ -9,8 +9,8 @@ app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-#from models import PipeLine1
-#migrate = Migrate(app, db)
+from models import PipeLine1
+migrate = Migrate(app, db)
 
 @app.route('/')
 def hello():
